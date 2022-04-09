b929a25e052d0715b471319439556a45a6dddca1
CREATIVECAPSULE\pranay.usgaonkar <pranay.usgaonkar.cci@gmail.com>
2022-04-09 15:21:19 +0530

    Modify Swagger of jokes app BE

--- remix-jokes/be/src/main.ts
+++ remix-jokes/be/src/main.ts
@@ -6,8 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Quotes Management')
-    .setDescription('Manage quotes created by a user')
+    .setTitle('Jokes App')
+    .setDescription('Manage Jokes created by a user')
     .setVersion('1.0')
     .addBearerAuth()
     .build();
